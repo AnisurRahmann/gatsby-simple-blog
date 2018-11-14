776f809c11fb70582afda5a7f22954665ac60660
Shakil <pshakilwizard@gmail.com>
2018-11-15 02:04:36 +0600

    initialized

---
+++ src/components/layout.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { css } from "react-emotion";
+import { Link, StaticQuery, graphql } from "gatsby";
+
+import { rhythm } from "../utils/typography";
+
+export default ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div
+        className={css`
+          margin: 0 auto;
+          max-width: 800px;
+          padding: ${rhythm(3)};
+          padding-top: ${rhythm(1.5)};
+        `}
+      >
+        <Link to={`/`}>
+          <h3
+            className={css`
+              margin-bottom: ${rhythm(2)};
+              display: inline-block;
+              font-style: normal;
+            `}
+          >
+            {data.site.siteMetadata.title}
+          </h3>
+        </Link>
+        <Link
+          to={`/about/`}
+          className={css`
+            float: right;
+          `}
+        >
+          About
+        </Link>
+        <Link
+          to={`/my-files/`}
+          className={css`
+            float: right;
+            padding: ${rhythm(1)};
+          `}
+        >
+          Files
+        </Link>
+        {children}
+      </div>
+    )}
+  />
+);
